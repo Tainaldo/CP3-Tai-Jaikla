f4d6c486ef3edf54c2bca704fde9738b750d50f3
Tai Jaikla <tyjaikla8794@gmail.com>
2022-04-07 16:53:03 +0700

    Create Exercise4_Tai_J.py

---
+++ Exercise4_Tai_J.py
@@ -0,0 +1,8 @@
+FE = 85
+GB = 72
+ICS = 69
+CP = 80
+print("Foundation English :",FE,"คะแนน")
+print("General Business :",GB,"คะแนน")
+print("Introduction to Computer Systems :",ICS,"คะแนน")
+print("Computer Programming :",CP,"คะแนน")
